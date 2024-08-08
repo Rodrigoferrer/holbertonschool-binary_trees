e4ce201c1d1bcaeed9cf5e6b7af3e0599703015e
Rodrigo <rodrigoferrer30@gmail.com>
2024-08-08 13:09:13 +0000

    1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -22,4 +22,12 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
         new_node->left = new_node;
         new_node->right = NULL;
         return new_node->left;
+	if (new_node->left == NULL)
+	{
+		return NULL;
+	}
+	if (new_node->right != NULL)
+	{
+		return NULL;
+	}
 }
