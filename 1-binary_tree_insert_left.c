98e49ca5d5b29d5aea8fb9f2103105476de30aef
Rodrigo <rodrigoferrer30@gmail.com>
2024-08-08 14:05:09 +0000

    1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,23 +12,22 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    if (parent == NULL)
-    {
+	if (parent == NULL)
+	{
         return NULL;
-    }
+	}
 
-    binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
-    if (new_node == NULL)
-    {
-        return NULL;
-    }
+	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+    	{
+        	return NULL;
+    	}
+    	new_node->parent = parent;
+	new_node->n = value;
+	new_node->left = NULL;
+ 	new_node->right = NULL;
 
-    new_node->parent = parent;
-    new_node->n = value;
-    new_node->left = NULL;
-    new_node->right = NULL;
+	parent->left = new_node;
 
-    parent->left = new_node;
-
-    return new_node;
+		return new_node;
 }
