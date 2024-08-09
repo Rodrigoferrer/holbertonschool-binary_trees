5eb91427977487c5493511b79e048d406f35bd7c
Rodrigo <rodrigoferrer30gmail.com>
2024-08-09 14:28:33 +0000

    4-binary_tree_is_leaf.c

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -10,7 +10,7 @@
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
         if (node->right == NULL && node->left == NULL)
-                return (0);
-        return (1);
+                return (1);
+        return (0);
         binary_tree_is_leaf(node);
 }
